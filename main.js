fce23b3a2448e21af6beb9047b5a40f4fb3052e9
madhu <madhumohanmadhumohan3@gmail.com>
2023-08-16 19:28:40 +0530

    last one1

--- main.js
+++ main.js
@@ -3,13 +3,12 @@ const filesystem = require("fs");
 const app = http.createServer();
 
 const readfile = (filepath, response) => {
-    console.log(filepath);
+  console.log(filepath);
   if (filesystem.existsSync(filepath)) {
-    filesystem.readFile(filepath,(error, data) => {
+    filesystem.readFile(filepath, (error, data) => {
       if (error) {
         console.log("the error is " + error);
-      }
-       else {
+      } else {
         response.end(data);
       }
     });
@@ -22,7 +21,7 @@ app.on("request", (request, response) => {
   if (request.url == "/") {
     request.url = "/index.html";
   } else if (request.url == "/favicon") {
-    request.url ="/favicon.ico";
+    request.url = "/favicon.ico";
   }
   let requesturl = request.url;
   console.log(requesturl);
@@ -30,17 +29,16 @@ app.on("request", (request, response) => {
     response.writeHead(200, { "Content-Type": "text/html" });
     readfile(`./pages${requesturl}`, response);
   } else if (requesturl.indexOf(".css") !== -1) {
-    console.log("the request came to the block"+requesturl+response);
+    console.log("the request came to the block" + requesturl + response);
     response.writeHead(200, { "Content-Type": "text/css" });
     readfile(`.${requesturl}`, response);
   } else if (requesturl.indexOf(".js") !== -1) {
     response.writeHead(200, { "Content-Type": "text/javascript" });
     readfile(`.${requesturl}`, response);
-  } else if (requesturl.indexOf('.ico')!==-1){
-     response.writeHead(200,{"Content-Type": "text/ico"});
-     readfile(`./pages${requesturl}`,response);
-  }
-  else {
+  } else if (requesturl.indexOf(".ico") !== -1) {
+    response.writeHead(200, { "Content-Type": "text/ico" });
+    readfile(`./pages${requesturl}`, response);
+  } else {
     console.log("check your code");
   }
 });
